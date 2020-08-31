5ef6275b16a42b958a05f3744cfdb4d50ae06ccf
Mark Servilla <mark.servilla@gmail.com>
2020-08-30 20:53:12 -0600

    Add tests for get_entity_date_created and get_entity_count

--- tests/test_pasta_api.py
+++ tests/test_pasta_api.py
@@ -12,6 +12,8 @@
 :Created:
     8/29/20
 """
+from datetime import datetime
+
 import pytest
 
 import counter.pasta_api as pasta_api
@@ -32,7 +34,29 @@ def test_get_pids():
 def test_get_entities():
     scope = "knb-lter-ble"
     newest = False
-    end = None
+    end = "2020-08-30T00:00:00"
     entities = pasta_api.get_entities(scope, newest, end)
     assert len(entities) != 0
+
+
+def test_get_entity_date_created():
+    rmd_url = (
+        "https://pasta.lternet.edu/package/data/rmd/eml/knb-lter-ble/1/1/"
+        "a1723e0e5f3c4881f1a7ede1b036aba6"
+    )
+    date_created = pasta_api.get_entity_date_created(rmd_url)
+    assert isinstance(date_created, datetime)
+
+
+def test_get_entity_count():
+    count_url = (
+        "https://pasta.lternet.edu/package/data/eml/knb-lter-ble/1/1/"
+        "a1723e0e5f3c4881f1a7ede1b036aba6"
+    )
+    start = "20200101T00:00:00"
+    end = "20200830T00:00:00"
+    count = pasta_api.get_entity_count(count_url, start, end)
+    assert isinstance(count, int)
+
+
 
